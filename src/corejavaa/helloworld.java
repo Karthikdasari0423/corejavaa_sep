c833f0e287aebeac07b202aa53ef8b815966c23d
karthik dasari <kardasar@cisco.com>
2021-10-13 12:52:00 +0000

    core javaa comit

---
+++ src/corejavaa/helloworld.java
@@ -0,0 +1,13 @@
+package corejavaa;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	System.out.println("hello world");
+
+	}
+
+}
+
